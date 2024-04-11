48118e4d5fb7bb111556b14e339cc2a540d7d7c7
“fariahatulmaria” <89478001+farihatulmaria@users.noreply.github.com>
2024-04-11 12:41:29 +0600

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,6 @@ const Footer = () => {
     return (
         <div className='container-fluid text-center bg-light p-5'>
             <p className='p-3'>@ All Right Reserved by Tour Planer</p>
-            
         </div>
     );
 };
